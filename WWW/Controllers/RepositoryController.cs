f68a83a6892c4343ec3015d52f181ec96b8098d4
Jason J. Rotello <jason@jrotello.com>
2014-03-08 13:33:53 -0600

    Imported existing API source from 2013 hackathon.

---
+++ WWW/Controllers/RepositoryController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WWW.Controllers
+{
+    public class RepositoryController : ApiController
+    {
+        //// GET api/repository
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/repository/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        public List<Models.@event> GetAllEvents()
+        {
+            System.Web.HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            var repo = new Models.Repository.EventsRepository();
+            return repo.RetrieveAll();
+        }
+
+        // GET api/repository/5
+        [HttpPost]
+        public void DeleteEvent([FromBody]Models.@event wwwEvent)
+        {
+            System.Web.HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            var repo = new Models.Repository.EventsRepository();
+            repo.DeleteEvent(wwwEvent);
+        }
+
+        // POST api/repository
+        [HttpPost]
+        public void CreateEvent([FromBody]Models.@event wwwEvent)
+        {
+            System.Web.HttpContext.Current.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            var repo = new Models.Repository.EventsRepository();
+            repo.CreateEvent(wwwEvent);
+        }
+    }
+}
---
+++ WWW/Models/Repository/EventsRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WWW.Models.Repository
+{
+    public class EventsRepository
+    {
+        WhatWhenWhereEntities ctx = new WhatWhenWhereEntities();
+
+        public void CreateEvent(Models.@event wwwEvent)
+        {
+            ctx.events.Add(new @event
+            {
+                NameOfEvent = wwwEvent.NameOfEvent,
+                StartDate = wwwEvent.StartDate,
+                EndDate = wwwEvent.EndDate,
+                EventAddress = wwwEvent.EventAddress,
+                Attendance = wwwEvent.Attendance,
+                EventSite = wwwEvent.EventSite,
+                HotelAddress = wwwEvent.HotelAddress,
+                HQHotel = wwwEvent.HQHotel,
+                RoomNights = wwwEvent.RoomNights,
+                Sponsor = wwwEvent.Sponsor
+            });
+            ctx.SaveChanges();
+        }
+        public List<Models.@event> RetrieveAll()
+        {
+            return ctx.events.ToList<Models.@event>();
+        }
+
+        public void UpdateEvent(Models.@event wwwEvent)
+        {
+            ctx.events.Remove(wwwEvent);
+            ctx.SaveChanges();
+        }
+
+        public void DeleteEvent(Models.@event wwwEvent)
+        {
+            ctx.events.Remove(wwwEvent);
+            ctx.SaveChanges();
+        }
+    }
+}
